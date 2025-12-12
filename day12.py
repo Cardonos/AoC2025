42b0ec4c0925b3fec9c0e5f08bb8b30913152932
Cardonos <lasse.stausberg@kit.edu>
2025-12-12 09:54:39 +0100

    12

---
+++ day12.py
@@ -0,0 +1,22 @@
+areas = []
+with open("Input/12.txt") as f:
+    k = 0
+    for line in f:
+        k+=1
+        if k > 30:
+            areas.append([line[:-1]])
+# Part 1
+# Lazy assumption: Every present is a solid 3x3 block, no tiling necessary
+# if the area fits enough 3x3 blocks for every present it obviously works
+# for some reason this is already the correct answer
+fit_ar = 0
+for area in areas:
+    splitar = area[0].split(":")
+    x_y_ar = splitar[0].split("x")
+    # Number of 3x3 blocks that fit in th area
+    full_area = (int(x_y_ar[0])//3)*(int(x_y_ar[1])//3)
+    # Number of presents of any shape needed
+    k = sum([int(i) for i in splitar[1].split()])
+    if full_area >= k:
+        fit_ar += 1
+print(fit_ar)
