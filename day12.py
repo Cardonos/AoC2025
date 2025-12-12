89c1e825b57aab1ba2be63f16fc31e118d50bd14
Cardonos <lasse.stausberg@kit.edu>
2025-12-12 09:55:38 +0100

    12

--- day12.py
+++ day12.py
@@ -13,10 +13,11 @@ fit_ar = 0
 for area in areas:
     splitar = area[0].split(":")
     x_y_ar = splitar[0].split("x")
-    # Number of 3x3 blocks that fit in th area
+    # Number of 3x3 blocks that fit in the area
     full_area = (int(x_y_ar[0])//3)*(int(x_y_ar[1])//3)
     # Number of presents of any shape needed
     k = sum([int(i) for i in splitar[1].split()])
+    # if number of 3x3 spaces in the area is bigger or equal than number of presents they all obviously fit
     if full_area >= k:
         fit_ar += 1
 print(fit_ar)
